6b23e266a0945302b8764dbbe0b87c6f0ec1f1a0
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-10-17 01:42:38 -0400

    Update test_projectile.py

--- test_projectile.py
+++ test_projectile.py
@@ -3,22 +3,26 @@ import projectile
 
 class ProjectileTests(unittest.TestCase):
 
-    def test_calc_xy(self):
-        """Test the calc_xy() function."""
+    def test_projectile_with_drag(self):
+        """Test the projectile_with_drag() function."""
 
-        # Set the initial conditions
-        angles = [30, 45, 60]
-        v_initial = 700
-        x_start = 0
-        y_start = 0
+        # Set the initial velocity
+        v_start = 700
 
         # Calculate the expected x and y positions
-        expected_x_list, expected_y_list = projectile.calc_xy(angles, [], [], v_initial, x_start, y_start)
+        expected_x_list, expected_y_list = projectile.projectile_with_drag(v_start)
 
         # Calculate the actual x and y positions
-        actual_x_list, actual_y_list = projectile.calc_xy(angles, [], [], v_initial, x_start, y_start)
+        actual_x_list, actual_y_list = projectile.projectile_with_drag(v_start)
 
-        # Assert that the expected and actual x and y positions are equal
-        for i in range(len(angles)):
+        # Assert that the expected and actual x and y positions are almost equal
+        for i in range(len(expected_x_list)):
             self.assertAlmostEqual(expected_x_list[i], actual_x_list[i], places=5)
             self.assertAlmostEqual(expected_y_list[i], actual_y_list[i], places=5)
+
+    def test_projectile_with_drag_invalid_input(self):
+        """Test the projectile_with_drag() function with invalid input."""
+
+        # Test with a negative initial velocity
+        with self.assertRaises(ValueError):
+            projectile.projectile_with_drag(-700)
