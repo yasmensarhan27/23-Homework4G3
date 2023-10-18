590179bd7a262321024154d19e39ce3ced6e9517
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-10-18 13:27:10 -0400

    Update projectile_unit_test.py

--- projectile_unit_test.py
+++ projectile_unit_test.py
@@ -1,10 +1,11 @@
-#import the necessary libraries to run the code
+##import the necessary libraries to run the code
 import math
 import unittest
-# Import the function you want to test from the module you created 
-from projectile_new import calc_xy 
+# Import the function you want to test from the module you created
+from projectile_new import calc_xy
 #create a class for the unittisting including 2 functions
 class TestProjectileMotion(unittest.TestCase):
+    #a function to test the calculation
     def test_calc_xy(self): #test the fuctuin that calculates the position (x and y indicies)
         # Test the calc_xy function with specific inputs and expected outputs
         angles = [int(math.radians(30)), int(math.radians(45))]
@@ -18,7 +19,8 @@ class TestProjectileMotion(unittest.TestCase):
         self.assertTrue(all(x) for x in x_list)
         self.assertTrue(all(y) for y in y_list)
         #--------------
-    def test_degrees_to_radians_conversion(self): #functuon to test the conversion
+    #functuon to test the conversion
+    def test_degrees_to_radians_conversion(self): 
         # Test the degrees to radians conversion function
         angle_degrees = [30, 45, 60]
         angle_radians = [math.radians(deg) for deg in angle_degrees]
