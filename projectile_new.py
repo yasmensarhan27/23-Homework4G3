0c801a7322020146939a9c817b6208d9c15fe86d
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-10-18 13:06:40 -0400

    Update projectile_new.py

--- projectile_new.py
+++ projectile_new.py
@@ -1,6 +1,6 @@
 #import the necessary libraries to run the code
 import math
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # import matplotlibrary to plot and call it as plt
 #define some constants
 g = 9.81 # m / s^2
 dt = 0.75 # time step in seconds
@@ -15,7 +15,7 @@ angle = [math.radians(degrees) for degrees in angle_degree] #convert launch angl
 x_list=[] #list to store x postions
 y_list=[] #list to store y postions
 #define function to calculate
-def calc_xy(angles,x_list, y_list, v_initital, x_start, y_start ):
+def calc_xy(angles,x_list, y_list, v_initital, x_start, y_start ):#cacluating the position
     for j in angles: # loop over launch angles
         vx = v_initial*math.cos(angle[j]) # intial x velocity
         vy = v_initial*math.sin(angle[j]) # initial y velocity
@@ -23,7 +23,7 @@ def calc_xy(angles,x_list, y_list, v_initital, x_start, y_start ):
         y=[y_start] # list to store y postions
         i = 0 # index for while loop
         while y[-1]>=0: # loop to calculate x and y positions
-            x.append(x[i]+vx*dt) # calculate and save                         
+            x.append(x[i]+vx*dt) # calculate and save
             y.append(y[i]+vy*dt) # calculate and save y
             vx=vx-const*dt*vx*math.sqrt((vx**2)+(vy**2)) # update x velocity
             vy=vy-dt*(g+const*vy*math.sqrt((vx**2)+(vy**2))) # update y velocity
@@ -40,10 +40,10 @@ def calc_xy(angles,x_list, y_list, v_initital, x_start, y_start ):
 colours=["purple","blue","green","yellow","orange","red"] # list of colours
 plt.figure(figsize=(8,6)) # set plot size
 for i in range(len(x_list)): # plotting trajectory for each launch angle
-  plt.scatter(list(map(lambda z: z/1000, x_list[i])),list(map(lambda z: z/1000, y_list[i])),
+    plt.scatter(list(map(lambda z: z/1000, x_list[i])),list(map(lambda z: z/1000, y_list[i])),
                 c=colours[i],s=1,label='θ = {}°'.format(angle_degree[i])) # colours and legends
 plt.xlabel('x (km)')
 plt.ylabel('y (km)')
 plt.legend()
 plt.title("Projectile Motion with Air Drag")
-plt.show()     
+plt.show()
