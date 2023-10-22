9ff0a09569b572488d805c7425512e45bcf80feb
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-10-21 21:22:21 -0400

    Update projectile_unit_test.py

--- projectile_unit_test.py
+++ projectile_unit_test.py
@@ -14,17 +14,17 @@ class TestProjectileMotion(unittest.TestCase):
         # Check if the returned x and y lists have the expected length
         self.assertEqual(len(x_list), len(angles))
         self.assertEqual(len(y_list), len(angles))
-
         # Check if the calculated values are reasonable (e.g., not empty)
-        self.assertTrue(all(x) for x in x_list)
-        self.assertTrue(all(y) for y in y_list)
+        self.assertTrue(all(x > 0 for x in x_list))
+        self.assertTrue(all(y < 0 for y in y_list))
         #--------------
     #functuon to test the conversion
     def test_degrees_to_radians_conversion(self):
         # Test the degrees to radians conversion function
         angle_degrees = [30, 45, 60]
         angle_radians = [math.radians(deg) for deg in angle_degrees]
-        self.assertEqual(angle_radians, [math.radians(30), math.radians(45), math.radians(60)])
+        self.assertEqual(angle_radians, angle_degrees)
 #----------
 if __name__ == '__main__':
     unittest.main()
+
